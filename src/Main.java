c30aebb781b9ad1e0406eb0adcadca70bee95dce
albanesi <alban.selimi@noseryoung.ch>
2023-03-09 22:03:56 +0100

    if-else excercise

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int drinkingAgeInAmerica=21;
+
+        int yourAge =16;
+
+        // we want to check if my age is bigger then the drinking age in america
+        if(drinkingAgeInAmerica>yourAge){
+            System.out.println("You can't drink");
+        }else{
+            System.out.println("You are allowed to drink");
+        }
+    }
+}
